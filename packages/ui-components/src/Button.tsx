11a487ea50b20cdfa07ba96ecd112d18b9c41757
WebSavva <websavvap@gmail.com>
2025-11-18 20:57:58 +0300

    React was updated

--- packages/ui-components/src/Button.tsx
+++ packages/ui-components/src/Button.tsx
@@ -2,8 +2,8 @@ import styled from "styled-components";
 import * as React from "react";
 
 type ButtonProps = {
-  size: "small" | "large";
-  isMobile: boolean;
+  $size: "small" | "large";
+  $isMobile: boolean;
 };
 
 const Button = styled.button<ButtonProps>`
@@ -13,18 +13,18 @@ const Button = styled.button<ButtonProps>`
   cursor: pointer;
   appearance: none;
   border: none;
-  padding: ${({ size }) => (size === "small" ? "5px 10px" : "15px 30px")};
-  font-size: ${({ size }) => (size === "small" ? "12px" : "16px")};
+  padding: ${({ $size }) => ($size === "small" ? "5px 10px" : "15px 30px")};
+  font-size: ${({ $size }) => ($size === "small" ? "12px" : "16px")};
   font-weight: 600;
   text-transform: uppercase;
-  ${({ size }) => size === "small" ? "border-top: 2px solid black;" : "border-bottom: 4px solid black;"}
+  ${({ $size }) => $size === "small" ? "border-top: 2px solid black;" : "border-bottom: 4px solid black;"}
   letter-spacing: 0.05em;
   box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
   transition: all 0.3s ease;
   &:hover {
     transform: translateY(-2px);
   }
-  ${({ isMobile }) => isMobile && "width: 100%;"}
+  ${({ $isMobile }) => $isMobile && "width: 100%;"}
 `;
 
 export default Button;
